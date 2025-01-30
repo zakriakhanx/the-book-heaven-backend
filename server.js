6aa89e52b9ab9b8eb48b5882d837cca6fac63f34
Zakria <zakriakhanz2003@gmail.com>
2025-01-30 14:08:19 +0500

    configure server to use environment variables for port and load dotenv

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import books from './routes/bookRoutes.js'
 import review from './routes/reviewRoutes.js'
 import connectDB from './config/db.js'
 import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
 
 connectDB
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
