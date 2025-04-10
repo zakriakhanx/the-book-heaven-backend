cf77004b10e8ef160e0b27a46b467888139d04bf
Zakria <zakriakhanz2003@gmail.com>
2025-04-10 21:50:31 +0500

    Add authorization middleware to getAllUsers route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { signIn, signUp, getAllUsers } from "../controllers/auth.controller.js";
+import { authorize, authorizeRole } from '../middleware/auth.middleware.js';
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', signIn);
-authRouter.get('/users', getAllUsers);
+authRouter.get('/users',authorize, authorizeRole('admin'), getAllUsers);
 
 export default authRouter;
